95c62ef1a74f1da0633044ad6de39484e091f759
Mousseauliam <158599547+Mousseauliam@users.noreply.github.com>
2025-10-20 18:52:50 -0400

    2 drivers

--- test_driver.py
+++ test_driver.py
@@ -4,30 +4,40 @@ from time import sleep
 
 # Test connexion I2C
 try:
-    driver = PCADriver(address=0x41, freq=50)
+    driver1 = PCADriver(address=0x40, freq=50)
+    driver2 = PCADriver(address=0x41, freq=50)
 except Exception as e:
     print(f"❌ Erreur d'initialisation: {e}")
     exit(1)
 
-servo0 = Servo(driver, channel=0)
-servo1 = Servo(driver, channel=1)
-servo2 = Servo(driver, channel=2)
-servo4 = Servo(driver, channel=4)
-servo5 = Servo(driver, channel=5)
-servo6 = Servo(driver, channel=6)
-servo8 = Servo(driver, channel=8)
-servo9 = Servo(driver, channel=9)
-servo10 = Servo(driver, channel=10)
+servo0 = Servo(driver1, channel=0)
+servo1 = Servo(driver1, channel=1)
+servo2 = Servo(driver1, channel=2)
+servo4 = Servo(driver1, channel=4)
+servo5 = Servo(driver1, channel=5)
+servo6 = Servo(driver1, channel=6)
+servo8 = Servo(driver1, channel=8)
+servo9 = Servo(driver1, channel=9)
+servo10 = Servo(driver1, channel=10)
+servo12 = Servo(driver2, channel=0)
+servo13 = Servo(driver2, channel=1)
+servo14 = Servo(driver2, channel=2)
+servo16 = Servo(driver2, channel=4)
+servo17 = Servo(driver2, channel=5)
+servo18 = Servo(driver2, channel=6)
+servo20 = Servo(driver2, channel=8)
+servo21 = Servo(driver2, channel=9)
+servo22 = Servo(driver2, channel=10)
 
-servos = [servo0, servo1, servo2, servo4, servo5, servo6, servo8, servo9, servo10]
+servos = [servo0, servo1, servo2, servo4, servo5, servo6, servo8, servo9, servo10, 
+          servo12, servo13, servo14, servo16, servo17, servo18, servo20, servo21, servo22]
 
 for servo in servos:
     servo.neutral()
         
 sleep(3)
 
-        
-        
-driver.close()
-sleep(0.5)  
+driver1.close()
+driver2.close()
+sleep(0.5)
 print("Ressources libérées")
